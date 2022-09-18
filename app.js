5ec63e80c7661e486121ad116a76b94f13cc46ef
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2022-09-18 10:19:01 +0600

    Parameterized Function

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+//==========Simple function==========//
+function myFunction(){
+    let x = 10;
+    let y = 10;
+    let total = x + y;
+    console.log("I'm simple function value:" + total);
+}
+myFunction();
 
+//==========Parameterized function==========//
+function newFunction (first, second){
+    let x = first;
+    let y = second;
+    let total = x + y;
+    console.log("I'm parameterized function value:" + total);
+}
+newFunction(2, 2);
+
+//==========Rest Parameters Functions==========//
+// function 
